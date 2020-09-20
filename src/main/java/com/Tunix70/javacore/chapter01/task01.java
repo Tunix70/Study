fad36c56d3575be5a2bb8f785e6e9f900ed0cdc5
Tunix70 <pestekhin70@yandex.ru>
2020-09-20 17:26:36 +0300

    Initialization of the project

---
+++ src/main/java/com/Tunix70/javacore/chapter01/task01.java
@@ -0,0 +1,7 @@
+package main.java.com.Tunix70.javacore.chapter01;
+
+public class task01 {
+    public static void main (String args[]){
+        System.out.println("Простая программа на Java.");
+    }
+}
