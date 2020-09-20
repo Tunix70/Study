69e3f748ffb809960489c7e424a5429233809a3c
Tunix70 <pestekhin70@yandex.ru>
2020-09-20 17:53:29 +0300

    Edition class name

--- src/main/java/com/Tunix70/javacore/chapter02/example1.java
+++ src/main/java/com/Tunix70/javacore/chapter02/example1.java
@@ -1,6 +1,6 @@
 package main.java.com.Tunix70.javacore.chapter02;
 
-public class task01 {
+public class example1 {
     public static void main (String args[]){
         System.out.println("Простая программа на Java.");
     }
