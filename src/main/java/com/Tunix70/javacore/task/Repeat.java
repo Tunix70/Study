10f0cbee44e833a0007aaddc302677702acc9c64
Tunix70 <pestekhin70@yandex.ru>
2020-10-22 00:11:46 +0700

    Исправил тип на boolean

--- src/main/java/com/Tunix70/javacore/task/Repeat.java
+++ src/main/java/com/Tunix70/javacore/task/Repeat.java
@@ -5,16 +5,17 @@ import java.util.TreeSet;
 
 public class Repeat {
 
-    public static void repeadNumber(int[]num){
+    public static boolean repeadNumber(int[]num){
         Set<Integer> numbers = new TreeSet<>();
         for (int i = 0; i <num.length; i++) {
             if(!numbers.add(num[i]))
-                System.out.println("Обнаружен дубликат числа " + num[i]);
+                break;
         }
+        return false;
     }
 
     public static void main(String[] args) {
-        int[]num = {4,5,5,6,6,8, 8};
+        int[]num = {4, 5, 5, 6, 6, 8, 8};
         repeadNumber(num);
     }
 }
