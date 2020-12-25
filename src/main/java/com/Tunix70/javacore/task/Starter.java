40b36ff23f131919cbb2d0a11943f212f2a186b4
TunixWork <pestekhin70@yandex.ru>
2020-12-25 15:57:42 +0700

    доделал Starter

--- src/main/java/com/Tunix70/javacore/task/Starter.java
+++ src/main/java/com/Tunix70/javacore/task/Starter.java
@@ -38,53 +38,30 @@ class Foo{
 public class Starter {
     public static void main(String[] args) throws InterruptedException {
         Foo foo = new Foo();
-        new Thread(new Thread2(foo)).start();
-        new Thread(new Thread3(foo)).start();
-        new Thread(new Thread1(foo)).start();
-//        CompletableFuture.runAsync
-    }
-}
-class Thread1 implements Runnable{
-        private Foo foo;
-        public Thread1(Foo foo){
-            this.foo = foo;
-        }
-         @Override
-         public void run() {
-             try {
-                 foo.printFirst();
-             } catch (InterruptedException e) {
-                 e.printStackTrace();
-             }
-        }
-}
-class Thread2 implements Runnable{
-        private Foo foo;
-        public Thread2(Foo foo){
-            this.foo = foo;
-        }
-         @Override
-         public void run() {
-             try {
-                 foo.printSecond();
-             } catch (InterruptedException e) {
-                 e.printStackTrace();
-             }
-         }
-}
-class Thread3 implements Runnable{
-        private Foo foo;
-        public Thread3(Foo foo){
-            this.foo = foo;
-        }
-        @Override
-        public void run() {
+        CompletableFuture.runAsync(() -> {
+            try {
+                foo.printFirst();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                foo.printSecond();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        CompletableFuture.runAsync(() -> {
             try {
                 foo.printThird();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+        });
+    }
 }
 
 
